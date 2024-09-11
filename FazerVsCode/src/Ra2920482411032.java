a3f346d062499b856e20912ea10ec17b5a6d6d0a
Riichardss <168204368+Riichardss@users.noreply.github.com>
2024-09-11 11:26:44 -0300

    [feat]primeiro commit com meus dados

---
+++ FazerVsCode/src/Ra2920482411032.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ra2920482411032 {
+   public static void main(String[] args) {
+    System.out.println("meu nome completo é Richard Alexsander Rodrigues");
+    System.out.println("meu RA 2920482411032");
+    System.out.println("tenho 26 anos");
+    System.out.println("Meu github é https://github.com/Riichardss");
+    System.out.println("Meu linkedin é  https://www.linkedin.com/in/richard-alexsander-a53878185/");
+    System.out.println("Curso Análise e Desenvolvimento de Sistemas, 2° Semestre 2024");
+   }
+
+
+    
+}
